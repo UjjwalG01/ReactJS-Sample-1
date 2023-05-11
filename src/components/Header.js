666396d1dc933de608c28358bbfd7439bf38f656
Ujjwal Giri <ug.online.st@gmail.com>
2023-05-11 13:04:59 +0545

    Final Version

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,19 @@
 import PropTypes from "prop-types";
 import Buttons from "./Buttons";
+import { useLocation } from "react-router-dom";
 
 const Header = ({ title, onAdd, showAdd }) => {
+  const location = useLocation();
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Buttons
-        color={showAdd ? "purple" : "green"}
-        text={showAdd ? "Close" : "Add"}
-        onClick={onAdd}
-      />
+      {location.pathname === "/" && (
+        <Buttons
+          color={showAdd ? "purple" : "green"}
+          text={showAdd ? "Close" : "Add"}
+          onClick={onAdd}
+        />
+      )}
     </header>
   );
 };
